4af5a85e5a19d1ba2b5a43a62ba26860fcd9fe42
mr.x <yingheting@gmail.com>
2019-11-13 15:33:49 +0800

    Server.java created

---
+++ src/Server/Server.java
@@ -0,0 +1,13 @@
+package Server;
+
+/**
+ * @author paulalan
+ * @create 2019/11/13 15:32
+ */
+public class Server
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test");
+	}
+}
